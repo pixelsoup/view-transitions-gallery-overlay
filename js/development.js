915814e8bf6a363efb6385496f8742522b1bea76
Geoff Schwartz <eieio@pixelsoup.com.au>
2024-01-04 09:54:01 +1100

    refactor classNames

--- js/development.js
+++ js/development.js
@@ -4,10 +4,8 @@ const overlayContent = document.getElementById("js-overlay-target");
 
 function toggleImageView(index) {
   const image = document.getElementById(`js-gallery-image-${index}`);
-
   // Apply a CSS class which contains the view-transition-name before animation starts
-  image.classList.add("gallery__image--active");
-
+  image.classList.add("galleryImageActive");
   const imageParentElement = image.parentElement;
 
   if (!document.startViewTransition) {
@@ -22,29 +20,23 @@ function toggleImageView(index) {
       moveImageToGrid(imageParentElement);
       return;
     }
-
     const transition = document.startViewTransition(() =>
       moveImageToGrid(imageParentElement)
     );
-
     // Wait for animation to complete
     await transition.finished;
-
     // Remove the class which contains the page-transition-tag after animation ends
-    image.classList.remove("gallery__image--active");
+    image.classList.remove("galleryImageActive");
   };
 }
 
 // Helper functions for moving the image around and toggling the overlay
-
 function moveImageToModal(image) {
   overlayWrapper.classList.add("overlay--active");
-
   overlayContent.append(image);
 }
 
 function moveImageToGrid(imageParentElement) {
   imageParentElement.append(overlayContent.querySelector("img"));
-
   overlayWrapper.classList.remove("overlay--active");
 }
